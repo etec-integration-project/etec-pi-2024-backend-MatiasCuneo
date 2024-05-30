368f0a3f67330c3629490a00ef81b0d803736012
Matsss3 <wyvmqrhjxflcvamrpe@gmail.com>
2024-05-29 23:09:16 -0300

    update

--- README.md
+++ README.md
@@ -13,3 +13,8 @@ deberia ser:
 2024-05-07 02:15:19.420019+00:00%
 
 o en ese estilo de fecha y hora
+
+
+curl:
+
+curl -X POST -H "Content-Type: application/json" -d '{"layersNum":3,"neuronsNum":2,"optimizer":"Adam","lossFunc":"MSE","alpha":2.5,"weightsId":"1234"}' http://localhost:5000/create_config/652132eb-82a0-406c-9c7c-7d3d0088c88b
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def getOnActive():
   return str(sql)
 
 @app.route("/usrconfig/<string:user_id>", methods=["GET"])
-def getConfigById(user_id):
+def getConfigByUserId(user_id):
   try:
     uuid_object = UUID(user_id, version=4)
   except ValueError:
@@ -26,7 +26,12 @@ def getConfigById(user_id):
   return jsonify({ "success": userConfig.to_json() }), 200
 
 @app.route("/create_config/<string:user_id>", methods=["POST"])
-def createConfigById(user_id):
+def createConfigByUserId(user_id):
+  try:
+    uuid_object = UUID(user_id, version=4)
+  except ValueError:
+    return jsonify({ "error": "ID is not a valid UUID" }), 400
+
   data = request.get_json()
 
   layersNum = data.get("layersNum")
@@ -54,6 +59,44 @@ def createConfigById(user_id):
   
   return jsonify({ "success": "User Config added successfully!" }), 200
 
+#Just for development purposes
+@app.route("/configs", methods=["GET"])
+def getAllConfigs():
+  try:
+    configs = UserConfig.query.all()
+    configs_json = list(map(lambda x: x.to_json(), configs))
+  except DataError as e:
+    return jsonify({ "error": e }), 500
+  
+  return jsonify({ "success": configs_json }), 200
+
+@app.route("/update_config/<string:id>", methods=["PATCH"])
+def updateConfigById(id):
+  try:
+    uuid_object = UUID(id, version=4)
+  except ValueError:
+    return jsonify({ "error": "ID is not a valid UUID" }), 400
+  
+  try:
+    config = UserConfig.query.filter_by(id=id).first()
+    if not config: return jsonify({ "error": "No Config found" }), 404
+  except DataError as e_1:
+    return jsonify({ "error": e }), 500
+  
+  data = request.json
+  config.layersNum = data.get("layersNum", config.layersNum)
+  config.neuronsNum = data.get("neuronsNum", config.neuronsNum)
+  config.optimizer = data.get("optimizer", config.optimizer)
+  config.lossFunc = data.get("lossFunc", config.lossFunc)
+  config.alpha = data.get("alpha", config.alpha)
+
+  try:
+    db.session.commit()
+  except DataError as e_2:
+    return jsonify({ "error": e_2 }), 500
+  
+  return jsonify({ "success": "Config updated successfully!" }), 200
+  
 if __name__ == "__main__":
   with app.app_context():
     db.create_all()
