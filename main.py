767896f336e81360a05e3cf96b6f5a77b3fb3389
m.cuneo <m.cuneo@alumno.etec.um.edu.ar>
2024-10-23 10:08:56 -0300

    ai update

--- main.py
+++ main.py
@@ -1,102 +1,29 @@
 from flask import request, jsonify
 from sqlalchemy import func
-from sqlalchemy.exc import DataError
 from config import app, db
-from models import UserConfig, Weights
-from uuid import UUID
+from etec-pi-2024-backend-MatiasCuneo.neural_network.img_recon import
+predict_digit
 
 @app.route("/", methods=["GET"])
 def getOnActive():
   sql = db.session.query(func.current_timestamp()).scalar()
   return str(sql)
 
-@app.route("/usrconfig/<string:user_id>", methods=["GET"])
-def getConfigByUserId(user_id):
-  try:
-    uuid_object = UUID(user_id, version=4)
-  except ValueError:
-    return jsonify({ "error": "ID is not a valid UUID" }), 400
-  
-  try:
-    userConfig = UserConfig.query.filter_by(userId=user_id).first()
-    if not userConfig: return jsonify({ "error": "No Config Found!" }), 404
-  except DataError as e:
-    return jsonify({ "error": e }), 500
+@app.route("/prediction", methods=["GET"])
+def getPrediction():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files['file']
 
-  return jsonify({ "success": userConfig.to_json() }), 200
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
 
-@app.route("/create_config/<string:user_id>", methods=["POST"])
-def createConfigByUserId(user_id):
-  try:
-    uuid_object = UUID(user_id, version=4)
-  except ValueError:
-    return jsonify({ "error": "ID is not a valid UUID" }), 400
+    if file:
+        prediction = predict_digit(imagen)
+        return jsonify({ "success": prediction }), 200
 
-  data = request.get_json()
+    return jsonify({ "error": "Something went wrong" }), 500
 
-  layersNum = data.get("layersNum")
-  neuronsNum = data.get("neuronsNum")
-  optimizer = data.get("optimizer")
-  lossFunc = data.get("lossFunc")
-  alpha = data.get("alpha")
-  weightsId = data.get("weightsId")
-
-  config = UserConfig(
-    userId=user_id,
-    layersNum=layersNum,
-    neuronsNum=neuronsNum,
-    optimizer=optimizer,
-    lossFunc=lossFunc,
-    alpha=alpha,
-    weightsId=weightsId
-  )
-
-  try:
-    db.session.add(config)
-    db.session.commit()
-  except DataError as e:
-    return jsonify({ "error": e }), 500
-  
-  return jsonify({ "success": "User Config added successfully!" }), 200
-
-#Just for development purposes
-@app.route("/configs", methods=["GET"])
-def getAllConfigs():
-  try:
-    configs = UserConfig.query.all()
-    configs_json = list(map(lambda x: x.to_json(), configs))
-  except DataError as e:
-    return jsonify({ "error": e }), 500
-  
-  return jsonify({ "success": configs_json }), 200
-
-@app.route("/update_config/<string:id>", methods=["PATCH"])
-def updateConfigById(id):
-  try:
-    uuid_object = UUID(id, version=4)
-  except ValueError:
-    return jsonify({ "error": "ID is not a valid UUID" }), 400
-  
-  try:
-    config = UserConfig.query.filter_by(id=id).first()
-    if not config: return jsonify({ "error": "No Config found" }), 404
-  except DataError as e_1:
-    return jsonify({ "error": e }), 500
-  
-  data = request.json
-  config.layersNum = data.get("layersNum", config.layersNum)
-  config.neuronsNum = data.get("neuronsNum", config.neuronsNum)
-  config.optimizer = data.get("optimizer", config.optimizer)
-  config.lossFunc = data.get("lossFunc", config.lossFunc)
-  config.alpha = data.get("alpha", config.alpha)
-
-  try:
-    db.session.commit()
-  except DataError as e_2:
-    return jsonify({ "error": e_2 }), 500
-  
-  return jsonify({ "success": "Config updated successfully!" }), 200
-  
 if __name__ == "__main__":
   with app.app_context():
     db.create_all()
--- neural_network/img_recon.py
+++ neural_network/img_recon.py
@@ -1,49 +1,10 @@
-import numpy as np
-import os
-import random
-import pandas as pd
-from PIL import Image
+import tensorflow as tf
+import keras
 
-# ========== PREPROCESADO DE IMAGENES ==========
-def load_dataset(dataset, path):
-    data = pd.read_csv(os.path.join(path, dataset) + '.csv')
+model = tf.keras.models.load_model('mnist_model.h5')
 
-    y = data['label'].values
-    X = data.drop('label', axis=1).values
+def predict_digit(image):
+    image = (255 - image.reshape((1, 28, 28, 1)).astype('float32')) / 255
+    predictions = model.predict(image)
+    return predictions.argmax()
 
-    return np.array(X), np.array(y).astype('uint8')
-
-def rotate_images(images, angle):
-    for image in images:
-        img = Image.fromarray(image.reshape(28, 28))
-        rotated_img = img.rotate(random.randint(-(angle), angle))
-        image = np.array(rotated_img)
-        image.flatten()
-    return images
-
-def translate_images(images):
-    shift = (random.randint(-5, 5), random.randint(-5, 5))
-    for image in images:
-        img = Image.fromarray(image.reshape(28, 28))
-        image = img.transform(img.size, Image.AFFINE, (1, 0, shift[0], 0, 1, shift[1]))
-    return images
-
-def preprocessing(images):
-    return translate_images(rotate_images(images, 9))
-
-X, y = load_dataset('train', '../mnist_dataset')
-
-keys = np.array(range(X.shape[0]))
-np.random.shuffle(keys)
-
-X = X[keys]
-y = y[keys]
-
-X = X.reshape(X.shape[0], -1).astype(np.float32)
-
-preprocessing(X)
-
-X = (X - 127.5) / 127.5
-
-# ========== TESTING ==========
-print(X[0])
